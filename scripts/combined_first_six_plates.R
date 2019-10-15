7696a649187373231a1e66a5ae51a92dca8bd692
Wang <wan333@csiro.au>
2019-10-16 09:44:12 +1100

    correct and improve the previous codes

--- scripts/combined_first_six_plates.R
+++ scripts/combined_first_six_plates.R
@@ -1,28 +1,40 @@
 library(tidyverse)
 
-read_csv("data/design/design_with_time.csv")
+design <- read_csv("data/design/design_with_time.csv")
 
-design_first_six_plate <- design_with_time %>% 
-  filter(Plate == 1 | Plate == 2 | Plate == 3 | Plate == 4 | Plate == 5 | Plate == 6)
+design_first_six_plate <- design %>% 
+  filter(Plate %in% 1:6) # logical vector operator: which element of the Plate are 1 to 6
 # just keep the first 6 plates (5184 observations: 8(raw)*12(col)*9(measurements/time points)*6(plates))
 
-filename1 <- "C:/Users/WAN333/Documents/Data school/starch-hydrolysis/data/tidydata/plate0"
+
+# design %>% filter(Plate %in% 1:6)
+
+filename1 <- "C:/Users/WAN333/Documents/Data school/starch-hydrolysis/data/tidydata/plate"
 
 filename2 <- '.csv' 
 
-plate <- c('2', '3', '4', '5', '6')
+plate <- c('02', '03', '04', '05', '06') 
 
-data_plate01 <- read_csv(paste0(filename1,'1',filename2)) 
+# alternative: plate <- c(02:06)
+
+data_first_six_plates <- read_csv(paste0(filename1,'01',filename2)) 
 # read the 1st spreadsheet et and assign it to a new dataframe
 
 for (plt in plate) {
   filename_real <- paste0(filename1, plt, filename2)
   print(filename_real)
   temp_data <- read_csv(filename_real)
-  data_first_six_plates <- bind_rows(data_plate01, temp_data)
+  data_first_six_plates <- bind_rows(data_first_six_plates, temp_data)
 }
 
-# why just plate01 and 06 ???
+# below is an ememple of taking one element in the vector(set a value for the 'counter'), 
+# and step through the loop, see what the loop is doing (called DEBUG)
+
+# plt <- plate[1]
+# filename_real <- paste0(filename1, plt, filename2)
+# print(filename_real)
+# temp_data <- read_csv(filename_real)
+# data_first_six_plates <- bind_rows(data_plate01, temp_data)
 
 data_first_six_plates 
 
