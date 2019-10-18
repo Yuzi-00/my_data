c0e5daf13d1685d242dfa7863ec4b8773f6eba8a
Wang <wan333@csiro.au>
2019-10-18 23:45:20 +1100

    correcting the calculation for HE

--- scripts/caculation_6P.R
+++ scripts/caculation_6P.R
@@ -7,9 +7,7 @@ cal_6P <- data_6P %>%
   mutate(OD = OD_sample - OD_blk, # caculate the OD (sample-blank)
          c = OD / Slope, # calculate the concentration
          c_nor = 10 * c / Mass, # calculate the concentration by normalising the mass at 10 mg
-         HE = c / 10 / 0.9 * 100) # calculate the hydrolysis extent
+         HE = c / (10 / 0.9) * 100) # calculate the hydrolysis extent
                                  # in theory, the HE should be 10/0.9 = 11 g/L
 
 write_csv(cal_6P, "data/tidydata/cal_6P.csv")
-
-
