183c707a18946c8fee3f420f4b6d9be318fbb058
Wang <wan333@csiro.au>
2019-10-24 22:33:08 +1100

    read in the hydrolysis tidy data for the first six plates

---
+++ scripts/combined_6P_with_previous_data.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+
+data_6P_initial <- read_csv("data/tidydata/data_6P_without_cal.csv")
+# read in the tidy data for the first six plates (before calculating)
+
+# to be continued after tidying the other dataset separately first
+
