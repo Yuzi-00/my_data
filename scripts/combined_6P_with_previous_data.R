bd53a33d8756c06ae350f9518e032224de9aeff6
Wang <wan333@csiro.au>
2019-10-28 22:46:31 +1100

    adding the amy data

--- scripts/combined_6P_with_previous_data.R
+++ scripts/combined_6P_with_previous_data.R
@@ -1,7 +1,14 @@
 library(tidyverse)
 
-data_6P_initial <- read_csv("data/tidydata/data_6P_without_cal.csv")
+data_6P_HE <- read_csv("data/tidydata/data_6P_without_cal.csv")
 # read in the tidy data for the first six plates (before calculating)
 
+amy <- read_csv("data/tidydata/previous_data/amy_ID_sample.csv")
+# read in the tidy data for the amylose content
+
+data_6P_HE_amy <- left_join(data_6P_HE, amy)
+# combining the data together
+
+# need to check the NA
 # to be continued after tidying the other dataset separately first
 
