50dea0730ff1f58ce12a85ae6c706daa71064e24
Wang <wan333@csiro.au>
2019-11-09 16:07:17 +1100

    add the fitted parameters

--- scripts/combined_6P_with_previous_data.R
+++ scripts/combined_6P_with_previous_data.R
@@ -18,7 +18,7 @@ write_csv(data_6P_HE_amy, "data/tidydata/HE_amy_6P.csv")
 # need to add the Amy% to C+ and C- (replace the NA) !!!!!!!!!!!!!!!!!!!!!!!
 
 
-##################################################### add master size #########################################################
+##################################################### add master size #######################################################
 
 
 master_size <- read_csv("data/tidydata/master_size.csv") # read in the master size dataset
@@ -26,6 +26,15 @@ master_size <- read_csv("data/tidydata/master_size.csv") # read in the master si
 HE_amy_size_6P <- left_join(data_6P_HE_amy, master_size) # combine these dataset together
 
 write_csv(HE_amy_size_6P, "data/tidydata/HE_amy_size_6P.csv")
+
+
+#################################################### add fitted parameters ###################################################
+
+parameters <- read_csv("analysis/fitted_parameters.csv")
+
+HE_amy_size_para_6P <- left_join(HE_amy_size_6P, parameters)
+
+write_csv(HE_amy_size_para_6P, "results/HE_amy_size_para_6P.csv")
 
 
 # to be continued after tidying the other dataset separately first
